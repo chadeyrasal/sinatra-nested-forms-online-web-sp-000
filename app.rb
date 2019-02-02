b9a49541f57711087904e7d015c71a4cd549f944
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-02-02 11:14:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,12 +3,6 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # this is the route the READ all the pirates
-#    get '/pirates' do
-#      @pirates = Pirate.all
-#      erb :'pirates/index'
-#    end
-
     get '/' do
       erb :'pirates/new'
     end
