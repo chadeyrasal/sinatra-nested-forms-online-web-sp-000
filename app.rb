7349d04e973121827ebb353e7540b9a93e08a8ed
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-02-02 10:43:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,10 +4,10 @@ module FormsLab
   class App < Sinatra::Base
 
     # this is the route the READ all the pirates
-    get '/pirates' do
-      @pirates = Pirate.all
-      erb :'pirates/index'
-    end
+#    get '/pirates' do
+#      @pirates = Pirate.all
+#      erb :'pirates/index'
+#    end
 
     get '/' do
       erb :index
